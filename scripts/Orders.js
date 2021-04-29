e30556a575666983d760a6400001a005c21d9b83
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-04-29 15:46:38 -0500

    Added costs to Orders.js

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -7,16 +7,43 @@ import {
 } from "./database.js";
 
 const buildOrderListItem = (order) => {
+  const paints = getPaints();
+  const interiors = getInteriors();
+  const technologies = getTechnologies();
+  const wheels = getWheels();
+
+  const foundPaint = paints.find((paint) => {
+    return paint.id === order.paintId;
+  });
+  let paintCost = foundPaint.price;
+
+  const foundInterior = interiors.find((interior) => {
+    return interior.id === order.interiorId;
+  });
+  let interiorCost = foundInterior.price;
+
+  const foundTechnology = technologies.find((technology) => {
+    return technology.id === order.technologyId;
+  });
+  let technologyCost = foundTechnology.price;
+
+  const foundWheels = wheels.find((wheelPair) => {
+    return wheelPair.id === order.wheelsId;
+  });
+  let wheelsCost = foundWheels.price;
+
+  let totalCost = paintCost + interiorCost + technologyCost + wheelsCost;
+
+  const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
   return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
-    </li>`;
+          Order #${order.id} costs ${costString}
+      </li>`;
 };
 
 export const Orders = () => {
-  /*
-          Can you explain why the state variable has to be inside
-          the component function for Orders, but not the others?
-      */
   const orders = getOrders();
 
   let html = "<ul>";
